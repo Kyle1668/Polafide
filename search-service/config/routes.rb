4c9198ee365f69237b131e6a37c2b8fd2da67628
Kyle1668 <kyledevinobrien1@gmail.com>
2018-03-10 13:47:38 -0800

    got basics of API working

--- search-service/config/routes.rb
+++ search-service/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :places
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    
+    resources :places
   end
 
 end
