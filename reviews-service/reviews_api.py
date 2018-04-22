8d5955ce78156ad25e7aa21205323d5301235623
Kyle1668 <kyledevinobrien1@gmail.com>
2018-04-22 13:10:03 -0700

    prog

--- reviews-service/reviews_api.py
+++ reviews-service/reviews_api.py
@@ -5,6 +5,15 @@ from flask import jsonify
 
 
 def make_request_url(place_id):
+    """[summary]
+
+    Arguments:
+        place_id {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
+
     request_url = "https://maps.googleapis.com/maps/api/place/details/json?"
     id_parameter = "placeid=" + place_id
     key_parameter = "&key=" + json.load(open("keys.json"))["places"]
@@ -12,6 +21,15 @@ def make_request_url(place_id):
 
 
 def get_reviews_from_api(place_id):
+    """[summary]
+
+    Arguments:
+        place_id {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
+
     url = make_request_url(place_id)
     print(url)
     request = requests.get(url).json()
