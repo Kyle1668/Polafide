579fc7f1d0481a0bb54e9b3a12e7fa2d42a6a425
Kyle1668 <kyledevinobrien1@gmail.com>
2018-05-31 08:25:39 -0700

    Deleted Rails Project

--- search-service/db/migrate/20180310215024_remove_updated_at_from_places.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUpdatedAtFromPlaces < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :places, :updated_at, :timestamp
-  end
-end
--- search-service/db/migrate/20180310215129_remove_created_at_from_places.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCreatedAtFromPlaces < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :places, :created_at, :timestamp
-    remove_column :places, :id, :integer
-  end
-end
